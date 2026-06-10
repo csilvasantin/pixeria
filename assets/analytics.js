d35c86860b161846f5132d678e41f0d8a28687be
Carlos Silva <csilvasantin@mac.lan>
2026-06-11 01:08:33 +0200

    fix: standardize Pixeria analytics tracking

--- assets/analytics.js
+++ assets/analytics.js
@@ -14,12 +14,20 @@
   });
 
   window.dataLayer.push({
+    'gtm.start': new Date().getTime(),
+    event: 'gtm.js'
+  });
+
+  window.dataLayer.push({
     event: 'pixeria.page_view',
     pixeria_path: window.location.pathname,
-    pixeria_title: document.title
+    pixeria_title: document.title,
+    pixeria_language: document.documentElement.lang || 'unknown'
   });
 
   var firstScript = document.getElementsByTagName('script')[0];
+  if (!firstScript || !firstScript.parentNode) return;
+
   var tag = document.createElement('script');
   tag.async = true;
   tag.src = 'https://www.googletagmanager.com/gtm.js?id=' + encodeURIComponent(containerId);
